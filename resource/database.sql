a46e7d63bdc302bb3b5c74f0cddf7d930b7641a5
Contrathetix <jonni.hanski@student.lut.fi>
2016-08-08 15:35:54 +0300

    cleanup

--- resource/database.sql
+++ resource/database.sql
@@ -3,24 +3,9 @@ PRAGMA FOREIGN_KEYS = on;
 CREATE TABLE Mod (
     name VARCHAR(511) UNIQUE NOT NULL,
     version VARCHAR(15) DEFAULT '1.0.0.0',
-    size INTEGER DEFAULT 0,
-    files INTEGER DEFAULT 0,
     ind INTEGER DEFAULT 0,
 
     CHECK (ind > -1),
 
     PRIMARY KEY (name)
 );
-
-CREATE TABLE Data (
-    name VARCHAR(31) UNIQUE NOT NULL,
-    value VARCHAR(4095) NOT NULL,
-
-    PRIMARY KEY (name)
-);
-
-INSERT INTO Data (name, value) VALUES ("pathMods", "D:\ModsMorrowind");
-INSERT INTO Data (name, value) VALUES ("pathGame", "D:\ModsMorrowind\Morrowind");
-INSERT INTO Data (name, value) VALUES ("pathData", "D:\ModsMorrowind\Morrowind\Data");
-INSERT INTO Data (name, value) VALUES ("pathLink", "D:\ModsMorrowind\Keening\SymlinkTree");
-INSERT INTO Data (name, value) VALUES ("pathSave", "D:\ModsMorrowind\Morrowind\Saves");
